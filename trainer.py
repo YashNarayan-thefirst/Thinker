ce3a07a9fcc4f41d453d5cf3eb2de91c56fe0eae
YashNarayan-thefirst <106808693+YashNarayan-thefirst@users.noreply.github.com>
2023-09-20 16:32:52 +0530

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -3,19 +3,11 @@ import numpy as np
 import tensorflow as tf
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Embedding, LSTM, Dense, Dropout, Bidirectional
-from keras.optimizers import Adam
-from keras.callbacks import EarlyStopping
 
 # Function to preprocess and train on a chunk of data
 def preprocess_and_train(data, model, tokenizer, max_sequence_length, batch_size):
     X_train, y_train = preprocess_data(data, tokenizer, max_sequence_length)
-    
-    early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
-    
-    history = model.fit(X_train, y_train, epochs=100, batch_size=batch_size,
-                        validation_split=0.2, verbose=2, callbacks=[early_stopping])
-    
+    history = model.fit(X_train, y_train, epochs=50, batch_size=batch_size, validation_split=0.2, verbose=2)
     return model
 
 # Function to preprocess a batch of data
@@ -32,12 +24,12 @@ def preprocess_data(data, tokenizer, max_sequence_length):
     return input_sequences, output_sequences
 
 # Main training loop
-chunk_size = 1000  # Set an appropriate chunk size
+chunk_size = 2000  # Set an appropriate chunk size
 batch_size = 32
 data = []
 
 with open('dataset.jsonl', 'r') as file:
-    tokenizer = Tokenizer()  # Initialize tokenizer once
+    tokenizer = None  # Initialize tokenizer once
     model = None  # Initialize model once
     max_sequence_length = None  # Initialize max_sequence_length once
 
@@ -46,7 +38,9 @@ with open('dataset.jsonl', 'r') as file:
         data.append(item)
 
         if len(data) >= chunk_size:
-            if tokenizer.word_index == {}:  # Build the tokenizer only once
+            if tokenizer is None:
+                # Build the tokenizer and get vocab size and max sequence length
+                tokenizer = Tokenizer()
                 tokenizer.fit_on_texts([item['input'] for item in data] + [item['output'] for item in data])
                 vocab_size = len(tokenizer.word_index) + 1
                 max_sequence_length = max(len(seq) for seq in data)
@@ -54,13 +48,12 @@ with open('dataset.jsonl', 'r') as file:
             if model is None:
                 # Build the model on the first chunk
                 model = tf.keras.Sequential([
-                    Embedding(input_dim=vocab_size, output_dim=128, input_length=max_sequence_length),
-                    Bidirectional(LSTM(128, return_sequences=True)),
-                    Dense(vocab_size, activation='softmax')
+                    tf.keras.layers.Embedding(input_dim=vocab_size, output_dim=128, input_length=max_sequence_length),
+                    tf.keras.layers.LSTM(128, return_sequences=True),
+                    tf.keras.layers.Dense(vocab_size, activation='softmax')
                 ])
 
-                optimizer = Adam(learning_rate=0.001)
-                model.compile(loss='sparse_categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+                model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
             # Train the model on the current chunk
             model = preprocess_and_train(data, model, tokenizer, max_sequence_length, batch_size)
@@ -70,20 +63,3 @@ with open('dataset.jsonl', 'r') as file:
 
 # Save the final model
 model.save('model.h5')
-
-def generate_output(input_text, model, tokenizer):
-    input_seq = tokenizer.texts_to_sequences([input_text])
-    input_seq = pad_sequences(input_seq, maxlen=max_sequence_length, padding='post')
-    predicted_seq = model.predict(input_seq)[0]
-    predicted_text = tokenizer.sequences_to_texts([np.argmax(predicted_seq, axis=-1)])[0]
-    return predicted_text
-
-# Load the model
-loaded_model = tf.keras.models.load_model('model.h5')
-
-# Inference function remains the same as in the previous code
-
-# Example of using the model for inference
-user_input = "What is the capital of France?"
-output_text = generate_output(user_input, loaded_model, tokenizer)
-print(f"Response: {output_text}")
